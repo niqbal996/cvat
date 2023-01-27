8acbd2884b649aba6322e21f39332d5c7b8bb447
Naeem Iqbal <niqbal@os-stud-5-u.ni.dfki>
2023-01-27 13:49:54 +0100

    Added launch script for cvat and nuclio functions

---
+++ launch_script.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+docker-compose -f docker-compose.yml -f docker-compose.override.yml -f components/serverless/docker-compose.serverless.yml up -d
+cd serverless
+./deploy_gpu.sh pytorch/facebookresearch/fcos/nuclio/
+./deploy_gpu.sh pytorch/saic-vul/hrnet/nuclio/
+./deploy_gpu.sh pytorch/ultralytics/yolov5_maize_onnx/
